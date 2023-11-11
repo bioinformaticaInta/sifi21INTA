673c7ba154ed9c4f5f23ca5c73fe875622a29b66
bioinformaticaInta <gonzalez.sergio@inta.gob.ar>
2023-11-10 21:25:26 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python
 
-#import FileDialog
-
-import sys
-import webbrowser
-import time
-import os
-
 import sifi_pipeline
 
-sifi = sifi_pipeline.SifiPipeline(bowtie_db="/home/sgonzalez/sifi21/testData/Arabidopsis_thaliana.TAIR10.cdna.all.fa",query_sequences="/home/sgonzalez/sifi21/testData/test.fasta", mode=0)
+sifi = sifi_pipeline.SifiPipeline(bowtie_db="/home/sgonzalez/sifi21/testData/Arabidopsis_thaliana.TAIR10.cdna.all.fa",query_sequences="/home/sgonzalez/sifi21/testData/test.fasta", mode=1)
 sifi.run_pipeline
